535e70c7fa0d7f70dbf1bf79f074d1dcfc8aa6ea
Ismail-2002 <smileplease2k27@gmail.com>
2021-12-16 13:40:29 +0530

    Completed mail and password validation

--- pages/signup.js
+++ pages/signup.js
@@ -1,72 +1,68 @@
-        let list=[];
-        function getData(){
-          event.preventDefault();
-          const username=document.getElementById("name").value;
-          const mail=document.getElementById("id").value;
-          const pass=document.getElementById("pass").value;
-          const cpass=document.getElementById("cpass").value;
+let list = [];
+function getData() {
+  console.group("getdata");
+  event.preventDefault();
 
-          let local=localStorage.getItem("signup");
+  const username = document.getElementById("name").value;
+  const mail = document.getElementById("id").value;
+  const pass = document.getElementById("pass").value;
+  const cpass = document.getElementById("cpass").value;
 
-          let parse=JSON.parse(local);
+  let local = localStorage.getItem("signup");
 
+  let parse = JSON.parse(local);
 
-          let already=false;
+  let already = false;
 
-          for(i=0; i<parse.length; i++){
-            let vanganum=parse[i].mailid;
-            if(mail==vanganum){
-              already=true;
-              break;
-            }
+  for (i = 0; i < parse.length; i++) {
+    let vanganum = parse[i].mailid;
+    if (mail == vanganum) {
+      already = true;
+      break;
+    }
+  }
+  if (already) {
+    alert("Mail ID already exist");
+    console.error("Mail ID already exist");
+    return;
+  }
 
-          }
-          if(already){
-            alert("Mail ID already exist")
-            
-          }
+  const obj = {
+    username: username,
+    mailid: mail,
+    password: pass,
+    confirmPassword: cpass,
+  };
 
+  if (pass != cpass) {
+    document.getElementById("invalid").innerText =
+      "Those passwords didn't match, Try again.";
+    return;
+  }
 
-        const obj = {
-          "username":username,
-          "mailid":mail,
-          "password":pass,
-          "confirmPassword":cpass
-        }
-        
-          if(pass != cpass){
-            document.getElementById("invalid").innerText="Those passwords didn't match, Try again."
-            return;
-          }
+  list.push(obj);
 
-          list.push(obj);
+  const string = JSON.stringify(list);
 
-          const string = JSON.stringify(list);
+  localStorage.setItem("signup", string);
 
-          localStorage.setItem("signup",string);
-        
-          window.location.href="./signin.html"
+  console.groupEnd();
+  window.location.href = "./signin.html";
+}
 
+function onLoad() {
+  console.group("onLoad");
+  let a = JSON.parse(localStorage.getItem("signup"));
+  if (a != null) {
+    list = a;
+  } else {
+    list = [];
+    localStorage.setItem("signup", JSON.stringify(list));
+  }
+  console.table(list);
 
-        }
-        
+  console.groupEnd();
+  return list;
+}
 
-        function myFunction() {
-          document.getElementById("invalid").innerText;
-        }
-
-        myFunction();
-
-        function onLoad(){
-          let a=JSON.parse(localStorage.getItem("signup"));
-          if(a != null){
-            list=a;
-          }
-          else{
-            list=[];
-            localStorage.setItem("signup",JSON.stringify(list));
-          }
-          return list;
-        }
-        
-        onLoad();
+onLoad();
