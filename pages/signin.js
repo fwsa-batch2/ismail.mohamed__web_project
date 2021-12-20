abade4761d1448ec119544a17c65aeaa98b7cd8b
Ismail-2002 <smileplease2k27@gmail.com>
2021-12-21 00:20:12 +0530

    added preview pages

--- pages/signin.js
+++ pages/signin.js
@@ -1,22 +1,61 @@
-        let list=[];
-        function getData(){
-          event.preventDefault();
-          const username=document.getElementById("name").value;
-          const pass=document.getElementById("pass").value;
+let list = [];
+function onLoad() {
+  console.group("onLoad");
+  let a = JSON.parse(localStorage.getItem("signup"));
+  if (a != null) {
+    list = a;
+  }
+  console.groupEnd();
+}
+
+onLoad();
+
+function getData() {
+  event.preventDefault();
+  const username = document.getElementById("name").value;
+  const pass = document.getElementById("pass").value;
+
+  const store = JSON.parse(localStorage.getItem("signup"));
+  for (i = 0; i < store.length; i++) {
+
+    // console.log(store.length);
+    // console.log(store[i].username);
+    // console.log( store[i].pass);
+
+    // console.log(store[i].username) 
+    // console.log(store[i].password) 
+
+    // console.log(username)
+    // console.log(pass)
 
 
-          const store = JSON.parse(localStorage.getItem("signup"));
+    if (store[i].username == username && store[i].password == pass) {
+      alert("Hi " + username + ", Welcome to Smyle Designs ");
+      window.location.href = "../index.html";
+      return;
+    } 
+    
+    else {
+      alert("Invalid username or password");
+    
+    }
+    
+  }
 
-            for(let i=0; i<store.length; i++){
-                if(store[i].username == username && store[i].password==pass){
-                    alert( "Hi " +username+ ", Welcome to Smyle Designs ");
-                    window.location.href="../index.html";
-                    break;
-                }
-                
-                else{
-                    alert("Invalid username or password");
-                    break;  
-                }
-            }
-        }
+
+
+}
+
+function showpassword(){
+  let pswd=document.getElementById("pass");
+  // let show=document.getElementById("cpass");
+
+  if(pswd.type === "password"){
+    pswd.type="text";
+  }
+  else{
+    pswd.type="Password";
+    
+
+  }
+}
--- pages/signup.js
+++ pages/signup.js
@@ -1,68 +1,83 @@
 let list = [];
-function getData() {
-  console.group("getdata");
-  event.preventDefault();
-
-  const username = document.getElementById("name").value;
-  const mail = document.getElementById("id").value;
-  const pass = document.getElementById("pass").value;
-  const cpass = document.getElementById("cpass").value;
-
-  let local = localStorage.getItem("signup");
-
-  let parse = JSON.parse(local);
-
-  let already = false;
-
-  for (i = 0; i < parse.length; i++) {
-    let vanganum = parse[i].mailid;
-    if (mail == vanganum) {
-      already = true;
-      break;
-    }
-  }
-  if (already) {
-    alert("Mail ID already exist");
-    console.error("Mail ID already exist");
-    return;
-  }
-
-  const obj = {
-    username: username,
-    mailid: mail,
-    password: pass,
-    confirmPassword: cpass,
-  };
-
-  if (pass != cpass) {
-    document.getElementById("invalid").innerText =
-      "Those passwords didn't match, Try again.";
-    return;
-  }
-
-  list.push(obj);
-
-  const string = JSON.stringify(list);
-
-  localStorage.setItem("signup", string);
-
-  console.groupEnd();
-  window.location.href = "./signin.html";
-}
 
 function onLoad() {
   console.group("onLoad");
   let a = JSON.parse(localStorage.getItem("signup"));
   if (a != null) {
     list = a;
-  } else {
-    list = [];
-    localStorage.setItem("signup", JSON.stringify(list));
   }
-  console.table(list);
-
   console.groupEnd();
-  return list;
 }
 
 onLoad();
+
+function getData() {
+  // console.group("getdata");
+  event.preventDefault();
+
+  let username = document.getElementById("name").value;
+  let mail = document.getElementById("id").value;
+  let pass = document.getElementById("pass").value;
+  let cpass = document.getElementById("cpass").value;
+
+  let check = already(mail);
+
+  if (check) {
+    document.getElementById("invalidmail").innerText =
+      "This mail is already exist";
+      return;
+  }
+
+  if (pass != cpass) {
+    document.getElementById("invalidpass").innerText =
+      "Those passwords didn't match, Try again.";
+    return;
+  } 
+  
+  else {
+    let obj = {
+      username: username,
+      mailid: mail,
+      password: pass,
+      confirmPassword: cpass,
+    };
+
+    list.push(obj);
+
+    // console.log(list);
+
+    let local = JSON.stringify(list);
+
+    localStorage.setItem("signup", local);
+
+    window.location.href = "./signin.html";
+  }
+}
+
+function already(current) {
+  let isExist = false;
+
+  for (i = 0; i < list.length; i++) {
+    // console.table(list[i]);
+    if (current == list[i].mailid) {
+      isExist = true;
+      break;
+    }
+  }
+  return isExist;
+
+}
+
+function showpassword(){
+  let pswd=document.getElementById("pass");
+  // let show=document.getElementById("cpass");
+
+  if(pswd.type === "password"){
+    pswd.type="text";
+  }
+  else{
+    pswd.type="Password";
+    
+
+  }
+}
