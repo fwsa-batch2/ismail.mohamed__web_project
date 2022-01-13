cd3938f64695a834e4e4f4297b81fbaa6fd08f58
Ismail-2002 <smileplease2k27@gmail.com>
2022-01-13 14:00:34 +0530

    Added Add Design Feature

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,28 @@ function search(){
   }
 }
 
+function addDesign(){
+  let getLs=JSON.parse(localStorage.getItem("details"));
+  let lsLen=getLs.length;
+  let emty="";
+
+  for(let i=0; i<lsLen; i++ ){
+    let index=getLs[i];
+    let prnt=`<div class="newly">
+               <img src="${index.image}" alt="smile designs logo" class="img-1" />
+                <div class="img-over">
+                 <div class="img-title">${index.titleName}</div>
+                  <p class="img-des">${index.category}</p>
+              </div>
+                <div>
+                  <img src="./assets/img/pro3.png" alt="profile pic" id="prof">
+                  <label>${index.designerName}</label>
+                </div>
+              </div>`
+    emty=emty+prnt;
+  }
+  document.getElementById("added").innerHTML=emty;
+
+}
+
+addDesign();
