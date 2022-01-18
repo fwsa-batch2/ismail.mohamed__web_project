6c9200a70cefc08c2f986ebc9476d803928edd75
Ismail-2002 <smileplease2k27@gmail.com>
2022-01-18 14:09:52 +0530

    fixed movie poster  filter feature

--- assets/js/moviefilter.js
+++ assets/js/moviefilter.js
@@ -1,20 +1,23 @@
 function filter(value){
   console.log(value)
-  
+  document.getElementById("flex").style.display="flex";
   
 
     if(value == "green"){
       document.getElementById("yellow").style.display = "none";
+      document.getElementById("green").style.display = "block";
       document.getElementById("black").style.display = "none";
     }
   
     else if(value == "yellow"){
       document.getElementById("green").style.display = "none";
+      document.getElementById("yellow").style.display = "block";
       document.getElementById("black").style.display = "none";
     }
 
     else if(value == "black"){
       document.getElementById("yellow").style.display = "none";
+      document.getElementById("black").style.display = "block";
       document.getElementById("green").style.display = "none";
     }
   
